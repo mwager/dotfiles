1478916c1709d0e12ce1ee576d179c44ce8cde65
Michael Wager <mail@mwager.de>
2013-12-19 20:35:57 +0100

    some cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,15 +4,23 @@ git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
-	source ~/.bash_profile
+
+    # hm ?
+    source ~/.bash_profile
+
+    echo 'Done. Please execute "source ~/.bash_profile" now.'
 }
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
-fi
+
+doIt
+
+# if [ "$1" == "--force" -o "$1" == "-f" ]; then
+# 	doIt
+# else
+# 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+# 	echo
+# 	if [[ $REPLY =~ ^[Yy]$ ]]; then
+# 		doIt
+# 	fi
+# fi
 unset doIt
+
